bbe6697e847d1877575d0026b829c1cbf65222ca
youssef-j3 <Y.Tawfiq18129@student.aast.edu>
2025-11-29 05:18:44 +0200

    Add files via upload

--- remind/lib/main.dart
+++ remind/lib/main.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
-import 'onboarding_screen.dart';
+import 'package:intl/intl.dart';
+import 'models/reminder_item.dart';
+import 'pages/reminder_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ThemeSwitcher());
+}
+
+class ThemeSwitcher extends StatefulWidget {
+  const ThemeSwitcher({super.key});
+
+  @override
+  State<ThemeSwitcher> createState() => _ThemeSwitcherState();
+}
+
+class _ThemeSwitcherState extends State<ThemeSwitcher> {
+  bool isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp(isDarkMode: isDarkMode, toggleTheme: toggleTheme);
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.isDarkMode, required this.toggleTheme});
+
+  final bool isDarkMode;
+  final VoidCallback toggleTheme;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ReMind',
+      debugShowCheckedModeBanner: false,
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color(0xfff3f4f7),
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(),
-      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xff121212),
+        useMaterial3: true,
+      ),
+      home: ReminderPage(isDarkMode: isDarkMode, toggleTheme: toggleTheme),
     );
   }
 }
